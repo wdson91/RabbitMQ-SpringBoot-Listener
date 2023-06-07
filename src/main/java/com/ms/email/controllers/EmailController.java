82bc9fc6daf41f8dd94d5f07efd016ad817d4ef7
Wdson Danrlei <wdson91@gmail.com>
2023-06-07 16:33:41 -0300

    finalizando projeto

---
+++ src/main/java/com/ms/email/controllers/EmailController.java
@@ -0,0 +1,30 @@
+package com.ms.email.controllers;
+
+import com.ms.email.dtos.EmailDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+@RestController
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+
+    @PostMapping("/sending-email")
+    public ResponseEntity<EmailModel> sendingEmail(@RequestBody EmailDto emailDto) {
+
+        EmailModel emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailDto, emailModel);
+        emailService.sendEmail(emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+    }
+
+}
